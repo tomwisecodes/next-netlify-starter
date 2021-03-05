34fa0ac14d251c6af5ba5d40483ac7bbc8fba6aa
Thomas Liam Wise <tom@putty.studio>
2021-03-05 12:42:05 +0000

    safsd

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,19 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Made with{" "}
+        <Image
+          src="/netliheart.svg"
+          alt="Netlify Logo"
+          className={styles.logo}
+          layout="fill"
+        />{" "}
+        for you
       </footer>
     </>
-  )
+  );
 }
